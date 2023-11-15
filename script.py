185c8f95b4fffad45dc4465708f7f223f001d044
Masamichi Kanakubo <masamichi.kanakubo@MacBook-Pro-6.local>
2023-11-15 16:09:53 +0900

    feature: make system proto0 countup

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import redis
 import ssl
 import os
 from dotenv import load_dotenv
+import time
 
 load_dotenv()
 
@@ -9,7 +10,7 @@ host_client = os.environ.get('HOST_CLIENT')
 redis_password = os.environ.get('REDIS_PASSWORD')
 redis_port = os.environ.get('REDIS_PORT')
 
-r = redis.Redis(
+redis_client = redis.Redis(
   host=host_client,
   port=33202,
   password=redis_password,
@@ -17,5 +18,12 @@ r = redis.Redis(
   ssl_cert_reqs=ssl.CERT_NONE
 )
 
-r.set('foo', 'bar')
-print(r.get('foo'))
+redis_client.set('count', 0)
+while True:
+    count = int(redis_client.get('count'))
+
+    count += 1
+    print(count)
+    redis_client.set('count', count)
+
+    time.sleep(10)
